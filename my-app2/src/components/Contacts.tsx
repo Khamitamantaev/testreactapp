72185e9ac6e47f399f2a45b2333d7e64b8a96ddb
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-01 10:13:31 +0300

    change list of contacts to top

--- my-app2/src/components/Contacts.tsx
+++ my-app2/src/components/Contacts.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
 
 const Contacts = (props: { contacts: any[]; }) => {
     return(
         <>
             <h1>Contacts</h1>
             <div>
-            {props.contacts.map((contact, index) => (
-                <div key={index}>{contact.name} with number {contact.phone}</div>
+            
+            {props.contacts.slice().reverse().map((contact, index) => (
+                <div key={index} >
+                    <div >Name: {contact.name} with Number: {contact.phone}</div>
+                    <Button>Delete Contact</Button>
+                    <Button>Update Contact</Button>
+                </div>
             ))}
         </div>
         </>
