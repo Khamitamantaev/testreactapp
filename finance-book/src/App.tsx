bf8310f84f386a3e8487bcd13d979bab49c2c5b4
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-02 18:42:32 +0300

    wallet page main

--- finance-book/src/App.tsx
+++ finance-book/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import WalletList from './components/WalletList';
 
 function App() {
   return (
     <div >
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <WalletList/>
     </div>
   );
 }
---
+++ finance-book/src/components/WalletList.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+
+const WalletList = () => {
+    return (
+        <>
+            <div className="min-h-full">
+                <nav className="bg-gray-800">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex items-center justify-between h-16">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                <img src={logo}  className="h-8 w-8" alt="logo" />
+                                </div>
+                                <div className="hidden md:block">
+                                    <div className="ml-10 flex items-baseline space-x-4">
+                                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                                        <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+                                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Reports</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Mobile menu, show/hide based on menu state. */}
+                    <div className="md:hidden" id="mobile-menu">
+                        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                            <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Reports</a>
+                        </div>
+                        
+                    </div>
+                </nav>
+                <header className="bg-white shadow">
+                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                    </div>
+                </header>
+                <main>
+                    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                        {/* Replace with your content */}
+                        <div className="px-4 py-6 sm:px-0">
+                            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                        </div>
+                        {/* /End replace */}
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default WalletList
